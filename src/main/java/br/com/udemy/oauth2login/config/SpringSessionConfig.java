b0a922399cc195788fc48ad769ea30196c5e10bb
Anderson Lemos Camelo <andersonlemos83@gmail.com>
2021-02-23 09:45:32 -0300

    Implementa sessão com Redis.

---
+++ src/main/java/br/com/udemy/oauth2login/config/SpringSessionConfig.java
@@ -0,0 +1,18 @@
+package br.com.udemy.oauth2login.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.session.data.redis.config.annotation.web.http.EnableRedisHttpSession;
+import org.springframework.session.web.context.AbstractHttpSessionApplicationInitializer;
+import org.springframework.session.web.http.HeaderHttpSessionIdResolver;
+import org.springframework.session.web.http.HttpSessionIdResolver;
+
+@Configuration
+@EnableRedisHttpSession
+public class SpringSessionConfig extends AbstractHttpSessionApplicationInitializer {
+
+    @Bean
+    public HttpSessionIdResolver httpSessionIdResolver() {
+        return HeaderHttpSessionIdResolver.xAuthToken();
+    }
+}
---
+++ src/main/java/br/com/udemy/oauth2login/gateway/http/UserResource.java
@@ -0,0 +1,25 @@
+package br.com.udemy.oauth2login.gateway.http;
+
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.servlet.http.HttpSession;
+import java.util.HashMap;
+import java.util.Map;
+
+@RestController
+@RequestMapping("/user")
+public class UserResource {
+
+    @GetMapping
+    @ResponseBody
+    public Map<String, String> token(HttpSession httpSession) {
+        Map<String, String> map = new HashMap<>();
+        map.put("token", httpSession.getId());
+        map.put("name", SecurityContextHolder.getContext().getAuthentication().getName());
+        return map;
+    }
+}
